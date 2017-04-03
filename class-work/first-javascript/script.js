2892fa419c51e937bc5cbcacae1b002c5c86a195
pcvoakes <pcvoakes@gmail.com>
2017-04-03 12:23:27 -0400

    Startup Matchmaker Homework

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,17 @@
+/*
+var num1 = 10 * 10;
+var num2 = 100 / 10;
+var num3 = 50 - 17;
+var num4  = 50 + 17;
+var myName = "Peter" + " " + "Voakes";
+
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(myName);
+*/
+
+var title = document.querySelector('#title')
+
+document.querySelector('#title').style.color = 'red'
