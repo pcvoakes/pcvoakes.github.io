f19742ea35d30c63e87a438d2f00338b959a4a5e
pcvoakes <pcvoakes@gmail.com>
2017-04-30 21:32:10 -0400

    Finally got the JavaScript to a working place!

---
+++ class-work/raindrops-example/js/app.js
@@ -0,0 +1,7 @@
+$('#drop-box').raindrops({
+		color: 'rgb(230, 5, 100)',
+		canvasHeight: 350,
+		waveHeight: 20,
+		frequency: 30,
+		rippleSpeed: .1,
+})
--- final-project/js/script.js
+++ final-project/js/script.js
@@ -1,42 +1,136 @@
+//Only using Tuna and Howler
 
-var baseTone = new Howl({
-  src: ['audio/base-tone-low.wav'],
-  autoplay: false,
-  loop: true,
-  volume: 0.5,
-});
+//set up OScillator node to start auiod stream
+// set up gain nodes for each of the Tuna effects
+//establish the graph
+//write a function to capture the cooridinates within the graph
 
-var toneWad = new Wad({
-	source:'../audio/base-tone-low.wav',
-	volume: 0.25,
-	loop: true,
-	delay: {
-		delayTime: .25,
-		wet: 0.25,
-		feedback: .75,
-	},
-	tremolo: {
-		shape: 'triangle',
-		magnitude: 1,
-		speed: 1.5,
-		attack: 0,
-	},
-	tuna: {
-		Overdrive: {
-			outputGain: 0.75,
-			drive: 0.5,
-			curveAmount: 1,
-			algorithmIndex: 2,
-			bypass: 0,
-		},
+//Establish audio context
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioCtx = new AudioContext();
+
+//Establish Tuna in Audio Context
+var tuna = new Tuna(audioCtx);
+
+//establish the Oscillator and Effect Nodes
+var oscillator = audioCtx.createOscillator();
+var chorus = new tuna.Chorus({
+	rate: 0,
+    feedback: .25,
+    delay: .00045,
+    bypass: 1,
+})
+var tremolo = new tuna.Tremolo({
+	intesnity: 10,
+	rate: 0,
+	stereoPhase: 180,
+	bypass: 1,
+})
+var phaser = new tuna.Phaser({
+	rate: 8,
+	depth: 0.75,
+	feedback: 0.6,
+	stereoPhase: 60,
+	baseModulationFrequency: 1000,
+	bypass: 1,
+})
+var distortion = new tuna.Overdrive({
+	outputGain: 0.5,
+	drive: 0.7,
+	curveAmount: 1,
+	algorithmIndex: 1,
+	bypass: 1,
+})
+
+var gateNodeStart = audioCtx.createGain();
+var gateNodeEnd = audioCtx.createGain();
+gateNodeEnd.gain.value = 0.25;
+
+
+//Connect Gate Node to complete audio chain
+gateNodeStart.connect(gateNodeEnd);
+gateNodeEnd.connect(audioCtx.destination);
+
+//Variable to set play button on or off
+var playing = false;
+oscillator.start();
+
+// mouse pointer coordinate variables
+var curX;
+var curY;
+var rate
+
+$('#start').click(function() {
+	if(playing === false) {
+		playing = true
+		buttonOn(this);
+	} else {
+		playing = false;
+		buttonOff(this);
 	}
 })
 
-var sineTest = new Wad({
-	source : 'sine',
-	volume: .5,
+//function to toggle effects on or off
+function effectToggle(effectNode) {
+	if (effectNode.bypass === 1) {
+		effectNode.bypass = 0;
+		gateNodeStart.connect(effectNode);
+		effectNode.connect(gateNodeEnd);
+	} else {
+		effectNode.bypass = 1;
+		effectNode.disconnect();
+	}
+}
+
+//button change CSS for on/off states
+function buttonOn(button) {
+	$(button).addClass('gradient');
+}
+
+function buttonOff(button) {
+	$(button).removeClass('gradient');
+}
+
+// Sets the effects button to toggle effects on or off
+$('#tremolo').click(effectToggle(tremolo));
+
+$('#chorus').click(function() {
+	effectToggle(chorus);
 })
 
+$('#phaser').click(function() {
+	effectToggle(phaser);
+})
+
+$('#overdrive').click(function() {
+	effectToggle(distortion);
+})
+
+//Sets playback of sound to start only when mouse moves over the effects grid
+$('#effects-grid').mouseenter(function() {
+	if (playing === true) {
+		oscillator.connect(gateNodeStart);
+		gateNodeEnd.connect(audioCtx.destination);
+	} 
+})
+
+$('#effects-grid').mouseout(function() {
+	if (playing === true) {
+		gateNodeEnd.disconnect(audioCtx.destination);
+	}
+})
+
+//dynamically change the effect values
+$('#effects-grid').mousemove(function() {
+	curX = event.pageX;
+	curY = event.pageY;
+	rate = (curX - 500) / 75;
+	tremolo.rate = rate
+	chorus.rate = rate
+	phaser.rate = rate
+})
+
+//JS library to establish layout of the grid
 var ctx = $('#effects-grid');
 
 var grid = new Chart(ctx, {
@@ -67,7 +161,7 @@ var grid = new Chart(ctx, {
 //var data = {
 //	xLabels: ['Zilch', 'Tame', 'Neat', 'Wild', 'Too Far'],
 //}
-
+/*
 $('#sine').click(function() {
 	sineTest.play({
 		tuna: {
@@ -80,37 +174,5 @@ $('#sine').click(function() {
 		}
 	})
 })
-
-$('#start').click(function () {
- 	// conditional needed to make the button an on-off toggle for the tone to play
- 	if (baseTone.playing() === false) {
- 		baseTone.play()
- 	} 
- 	else {
- 		baseTone.stop()
- 	}
- 	console.log(baseTone)
-})
-
-$('#start-wad').click(function () {
-	toneWad.play();
-})
-
-$('#filter').click(function () {
-	var tuna = new Tuna(Howler.ctx);
-	var phaser = new tuna.Phaser({
-	rate: 1.2,
-	depth: 0.7,
-	feedback: 0.3,
-	stereoPhase: 70,
-	baseModulationFrequency: 700,
-	bypass: 0,
-	});
-	var input = Howler.ctx.createGain();
-	var output = Howler.ctx.createGain();
-	input.connect(phaser);
-	phaser.connect(output);
-	console.log(phaser);
-	console.log(input);
-})
+*/
 
